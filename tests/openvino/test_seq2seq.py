6182779d438f556aea7e689b646b3c05c637786f
Riffat Khan <riffatk342@gmail.com>
2025-12-04 08:46:36 +0400

    Update tests for modified tiny minicpmo

--- tests/openvino/test_seq2seq.py
+++ tests/openvino/test_seq2seq.py
@@ -623,8 +623,8 @@ class OVModelForVisualCausalLMIntegrationTest(unittest.TestCase):
         ov_model.config.eos_token_id = None
         transformers_model.config.eos_token_id = None
         ov_model.generation_config.do_sample = False
-        # minicpmo diverges after 20 tokens
-        tokens_to_generate = 20 if model_arch == "minicpmo" else 30
+        # minicpmo diverges after 5 tokens
+        tokens_to_generate = 5 if model_arch == "minicpmo" else 30
         gen_config = GenerationConfig(
             max_new_tokens=tokens_to_generate,
             min_new_tokens=tokens_to_generate,
